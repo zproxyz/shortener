3412d3aff56b30c6168b9f8cdaf6ef8d8deb85e8
zproxyz <zproxyz@gmail.com>
2018-09-23 16:05:46 +0300

    Added LinkTest

---
+++ src/tests/_data/link_stat.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Snow
+ * Date: 22.09.2018
+ * Time: 20:39
+ */
+return [
+    [
+        'link_id'=> '1',
+        'datetime' => '2018-09-22 18:07:38',
+        'ip' => '172.20.0.1',
+        'user_agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0',
+        'os' => 'Windows',
+        'os_version' => '10',
+        'browser' => 'Firefox',
+        'browser_version' => '62.0',
+    ],
+];
--- src/tests/unit/models/LinkTest.php
+++ src/tests/unit/models/LinkTest.php
@@ -3,9 +3,9 @@
 namespace tests\models;
 
 use shortener\fixtures\LinkFixture;
+use shortener\fixtures\LinkStatFixture;
 use shortener\fixtures\UserFixture;
 use shortener\forms\CreateUrlForm;
-use shortener\forms\LoginForm;
 use shortener\models\Link;
 use shortener\services\ShortenerService;
 
@@ -30,6 +30,10 @@ class LinkTest extends \Codeception\Test\Unit
                 'class' => LinkFixture::class,
                 'dataFile' => codecept_data_dir() . 'link.php',
             ],
+            'link_stat' => [
+                'class' => LinkStatFixture::class,
+                'dataFile' => codecept_data_dir() . 'link_stat.php',
+            ],
         ]);
     }
 
@@ -43,7 +47,31 @@ class LinkTest extends \Codeception\Test\Unit
     public function testLinkCreate()
     {
         $form = new CreateUrlForm(['url' => 'https://test.dev/']);
-        $link = Link::create($form->url,\Yii::$app->security->generateRandomString(12),$form->expiration);
-        $this->assertEquals('https://test.dev/',$link->url);
+        $link = Link::create($form->url, \Yii::$app->security->generateRandomString(12), $form->expiration);
+        $this->assertEquals('https://test.dev/', $link->url);
     }
+
+    public function testLinkisActive()
+    {
+        $link = Link::findOne(1);
+        $this->assertTrue($link->isActive());
+    }
+
+    public function testLinkUpdateCounter()
+    {
+        $link = Link::findOne(1);
+        $link->updateCounter();
+        $this->assertEquals($link->counter, 1);
+    }
+
+    public function testVisitUrl()
+    {
+        $link = Link::findOne(1);
+        $ip = '172.20.0.1';
+        $userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0';
+        $this->shortenerService->visitUrl($link,$ip,$userAgent);
+        $this->assertEquals($link->counter, 1);
+        $this->assertEquals(\count($link->linkStats),2);
+    }
+
 }
